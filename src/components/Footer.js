99c726ab8d49401708aeb4556b65ba69fdd58fee
Uzoezi <uodueme@gmail.com>
2023-02-19 04:42:50 +0100

    More changes to app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@
 function Footer(props){
     return(
         <footer style={{marginTop: "60px"}}>
-        {props.empty ? <p style={{color: props.mode ? "black" : "white" }}>Add a todo</p> : <p style={{color: props.mode ? "black" : "white" }}>Drag and drop to record list</p> }
+        {props.empty ? <p style={{color: props.mode ? "black" : "white" }}>Press enter to add a todo</p> : <p style={{color: props.mode ? "black" : "white" }}>Drag and drop to record list</p> }
         </footer>
     )
 }
